16be762c97ac38f87a06678fa1ae542af58e55ae
Phil <phillipnitskiy@gmail.com>
2019-03-26 09:47:33 +0300

    Add files via upload
    Initial

---
+++ AzureStorageDemo/AzureStorage/ImageBlobRepository.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureStorage
+{
+	public class ImageBlobRepository
+	{
+		private readonly string connectionString =
+			ConfigurationManager.AppSettings["StorageAccount.ConnectionString"];
+
+        private readonly string containerName = "images";
+
+        public async Task<CloudBlobContainer> GetCludBlobContainerAsync()
+        {
+            // Get the reference to a cloud storage account
+
+            // Create a cloud blob client
+
+            // Get a reference to a blob container
+
+            return (CloudBlobContainer) null;
+        }
+
+        public async Task<CloudBlockBlob> GetCloudBlockBlobAsync(string blobName)
+        {
+            CloudBlobContainer blobContainer = await this.GetCludBlobContainerAsync();
+
+            // Get a reference to a block blob
+
+            return (CloudBlockBlob) null;
+        }
+
+        public async Task UploadBlobFromFileAsync(string blobName, string path)
+        {
+            CloudBlockBlob blob = await GetCloudBlockBlobAsync(blobName);
+
+            // Upload a file to blob storage
+            await blob.UploadFromFileAsync(path);
+        }
+
+        public async Task UploadBlobFromStramAsync(string blobName, Stream stream)
+        {
+            CloudBlockBlob blob = await GetCloudBlockBlobAsync(blobName);
+
+            // Upload a file to blob storage using stream
+        }
+
+        public async Task UploadBlobTextAsync(string blobName, string content)
+        {
+            CloudBlockBlob blob = await GetCloudBlockBlobAsync(blobName);
+
+            // Upload text to blob storage
+        }
+
+        public async Task UploadBlobWithMetadataAsync(string blobName, string content, string title)
+        {
+            CloudBlockBlob blob = await GetCloudBlockBlobAsync(blobName);
+
+            // Add metadata to a blob
+
+            // Upload text to blob storage
+        }
+
+        public async Task UploadBlobWithContentTypeAsync(string blobName, Stream stream, string contenType)
+        {
+            CloudBlockBlob blob = await GetCloudBlockBlobAsync(blobName);
+
+            // Set the ContentType of the blob
+
+            // Upload a file to blob storage using stream
+        }
+
+        public async Task UploadBlobWithSmallFileAsync(string blobName, Stream stream)
+        {
+            CloudBlockBlob blob = await GetCloudBlockBlobAsync(blobName);
+
+            // Upload a file to blob storage using stream
+        }
+
+        public async Task UploadBlobWithLargeFileAsync(string blobName, Stream stream)
+        {
+            CloudBlockBlob blob = await GetCloudBlockBlobAsync(blobName);
+
+            // Upload a file to blob storage using stream
+        }
+
+        public async Task GetListOfBlobsAsync()
+		{
+            CloudBlobContainer blobContainer = await this.GetCludBlobContainerAsync();
+
+			// Get a reference to blobs
+
+			foreach (IListBlobItem blobItem in blobItems)
+			{
+				// Get the blob reference
+				CloudBlockBlob blob = (CloudBlockBlob) blobItem;
+
+			}
+		}
+	}
+}
---
+++ AzureStorageDemo/AzureStorageTests/ImageBlobRepositoryTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureStorage.Tests
+{
+    [TestClass]
+	public class ImageBlobRepositoryTests
+	{
+        [TestMethod]
+        public async Task GetCludBlobContainerAsync()
+        {
+            var repository = new ImageBlobRepository();
+
+            await repository.GetCludBlobContainerAsync();
+        }
+
+        [TestMethod]
+        public async Task GetCloudBlockBlobAsync()
+        {
+            var repository = new ImageBlobRepository();
+
+            await repository.GetCloudBlockBlobAsync("test");
+        }
+
+        [TestMethod]
+		public async Task UploadBlobFromFileAsync()
+		{
+			var repository = new ImageBlobRepository();
+
+            await repository.UploadBlobFromFileAsync("from_file", @"D:\epam.jpg");
+        }
+
+		[TestMethod]
+		public async Task UploadBlobFromStramAsync()
+		{
+			var repository = new ImageBlobRepository();
+
+            using (var fileStream = new FileStream(@"D:\epam.jpg", FileMode.Open))
+            {
+                await repository.UploadBlobFromStramAsync("from_stream", fileStream);
+            }
+        }
+
+        [TestMethod]
+        public async Task UploadBlobTextAsync()
+        {
+            var repository = new ImageBlobRepository();
+
+            var content = "If vegetarians eat vegetables, what do humanitarians eat?";
+
+            await repository.UploadBlobTextAsync("from_text", content);
+        }
+
+        [TestMethod]
+        public async Task UploadBlobWithMetadataAsync()
+        {
+            var repository = new ImageBlobRepository();
+
+            var content = "I used to breed rabbits. Then I realized they can handle it themselves.";
+            var title = "Dummy title";
+
+            await repository.UploadBlobWithMetadataAsync("with_meta", content, title);
+        }
+
+        [TestMethod]
+        public async Task UploadBlobWithContentTypeAsync()
+        {
+            var repository = new ImageBlobRepository();
+
+            var contentType = "image/jpeg";
+
+            using (var fileStream = new FileStream(@"D:\epam.jpg", FileMode.Open))
+            {
+                await repository.UploadBlobWithContentTypeAsync("with_content_type", fileStream, contentType);
+            }
+        }
+
+        [TestMethod]
+        public async Task UploadBlobWithSmallFileAsync()
+        {
+            var repository = new ImageBlobRepository();
+
+            using (var fileStream = new FileStream(@"D:\small.jpg", FileMode.Open))
+            {
+                await repository.UploadBlobWithSmallFileAsync("small", fileStream);
+            }
+        }
+
+        [TestMethod]
+        public async Task UploadBlobWithLargeFileAsync()
+        {
+            var repository = new ImageBlobRepository();
+
+            using (var fileStream = new FileStream(@"D:\large.jpg", FileMode.Open))
+            {
+                await repository.UploadBlobWithLargeFileAsync("large", fileStream);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetListOfBlobsAsync()
+        {
+            var repository = new ImageBlobRepository();
+
+            await repository.GetListOfBlobsAsync();
+        }
+    }
+}
